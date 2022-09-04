4c887da1753fa4cfdeaa6fea4273fdd37de0b4ea
Dimas Shofa Gunarso <55543999+Dimasshogun@users.noreply.github.com>
2022-09-04 17:32:30 +0700

    tutor #1: Hello World Unity

---
+++ Assets/Scripts/HelloWordUI.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWordUI : MonoBehaviour
+{
+    public string MyText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Debug.Log("Hello World !!!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    void OnGUI(){
+        GUILayout.BeginArea(new Rect(10,10,200,200));
+        GUILayout.Label(MyText);
+        GUILayout.EndArea();
+    }
+}
+
+// fungsi update() akan dijalankan terus menerus setiap frame.
+// 
